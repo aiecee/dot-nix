71cdf42526fcbeaca89d0fe35bce3f2b81fad6dd
Matt Collinge <mattycov@googlemail.com>
2023-11-26 22:28:16 +0000

    derop on package name

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,6 @@
   catppuccin-sddm = pkgs.libsForQt5.callPackage ./catppuccin-sddm.nix { };
   bemenu-pipewire = pkgs.callPackage ./bemenu-pipewire.nix { };
   dmenu-pipewire = pkgs.callPackage ./dmenu-pipewire.nix { };
-  dmenu-pipewire = pkgs.callPackage ./dmenu-power.nix { };
+  dmenu-power = pkgs.callPackage ./dmenu-power.nix { };
 
 }
