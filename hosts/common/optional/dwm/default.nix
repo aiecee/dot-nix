4642b147c15b3f717aa76254780acf79a819f510
Matt Collinge <mattycov@googlemail.com>
2023-11-26 22:42:03 +0000

    dwm refactor

--- hosts/common/optional/dwm/default.nix
+++ hosts/common/optional/dwm/default.nix
@@ -5,10 +5,7 @@
     enable = true;
     package = pkgs.unstable.dwm.overrideAttrs (old: rec {
       patches = [
-      (fetchpatch {
-        url = "https://raw.githubusercontent.com/aiecee/dwm/patched/dwm-nixos.diff";
-        sha256 = "";
-      })
+        ./dwm-nixos.diff
       ];
     });
   };
