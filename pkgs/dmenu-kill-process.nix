805720f291bbc756689edcefe0675e1272df8038
Matt Collinge <mattycov@googlemail.com>
2023-12-04 21:33:49 +0000

    dmenu-kill-process lint fixes

--- pkgs/dmenu-kill-process.nix
+++ pkgs/dmenu-kill-process.nix
@@ -4,12 +4,12 @@ writeShellApplication {
   name = "dmenu-kill-process";
   runtimeInputs = [ dmenu ];
   text = ''
-    $PROCESS=$(ps -a -u $USER -o pid,comm --no-headers | \
+    PROCESS=$(ps -a -u "$USER" -o "pid,comm" --no-headers | \
       dmenu -i -p "Kill" -l 10 "$@" |\
       awk '{print $1" "$2}');
 
     if [[ $PROCESS != "" ]]; then
-      $CONFIRM=$(echo -e "Yes/No" | \
+      CONFIRM=$(echo -e "Yes\nNo" | \
         dmenu -i -p "Are you sure?" "$@");
 
       if [[ $CONFIRM == "Yes" ]]; then
