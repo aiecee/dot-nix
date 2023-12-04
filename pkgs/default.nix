6d77db1c1291b98f14b634b91526f70e4257cde8
Matt Collinge <mattycov@googlemail.com>
2023-12-04 21:27:50 +0000

    dmenu-kill-process to be merged

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,6 @@
   bemenu-pipewire = pkgs.callPackage ./bemenu-pipewire.nix { };
   dmenu-pipewire = pkgs.callPackage ./dmenu-pipewire.nix { };
   dmenu-power = pkgs.callPackage ./dmenu-power.nix { };
+  dmenu-kill-process = pkgs.callPackage ./dmenu-kill-process.nix { };
 
 }
