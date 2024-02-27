cb2ef50aa278ec63335905ffa436ac2c8fe82638
Matt Collinge <mattycov@googlemail.com>
2024-02-27 21:55:57 +0000

    update bemenu colors

--- home/matt/features/desktop/wayland/hyprland/scripts.nix
+++ home/matt/features/desktop/wayland/hyprland/scripts.nix
@@ -4,7 +4,7 @@ let
   inherit (config.colorScheme) palette;
   inherit (config.rice.fonts.monospace) family;
   bemenu-args = ''
-    -W 0.3 -l 10 -B 2 -R 8 -n -c --fn "${family} 12" --bdr "#${palette.base03}" --tb "#${palette.base00}" --fb "#${palette.base00}" --nb "#${palette.base00}" --hb "#${palette.base00}" --ab  "#${palette.base00}" --tf "#${palette.base0A}" --ff "#${palette.base05}" --nf "#${palette.base05}" --hf "#${palette.base0A}" --af "#${palette.base05}"
+    -W 0.3 -l 10 -B 2 -R 8 -n -c --fn "${family} 12" --bdr "#${palette.base0A}" --tb "#${palette.base00}" --fb "#${palette.base00}" --nb "#${palette.base00}" --hb "#${palette.base00}" --ab  "#${palette.base00}" --tf "#${palette.base0A}" --ff "#${palette.base05}" --nf "#${palette.base05}" --hf "#${palette.base0A}" --af "#${palette.base05}"
   '';
 in
 {
